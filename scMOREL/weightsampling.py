b96ed7a0f7bcf36652296e31013d9013e7184b11
Jyu475 <2916080221@qq.com>
2026-06-26 23:40:03 +0800

    Update weightsampling.py

--- scMOREL/weightsampling.py
+++ scMOREL/weightsampling.py
@@ -6,8 +6,8 @@ def Weighted_Sampling(train_label = None):
         [(train_label == t).sum() for t in torch.unique(train_label, sorted=True)])
     weight = 1. / class_sample_count.float()
     samples_weight = torch.tensor([weight[t] for t in train_label])
-    sampler = Data.WeightedRandomSampler(samples_weight,#每个样本的权重 
-                                         len(samples_weight))# 采样总数
+    sampler = Data.WeightedRandomSampler(samples_weight,
+                                         len(samples_weight))
 
     return sampler
 
